ada95490a506d38606f7d75e4327c65508ae023a
qingwen.dong <qingwen.dong@naxions.com>
2021-10-20 20:45:31 +0800

    port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.use(cookieParser())
     app.enableCors()
-    await app.listen(3000)
+    await app.listen(3066)
 }
 bootstrap()
